a11753ce344cce6c13607e64d5508c46d0be4da1
fribeiro1 <fernando.ribeiro@upic.com.br>
2018-11-18 15:40:49 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# springframeworksample
+Sample Spring Framework Application
+
+This project contains versions of a sample Spring Framework application for multiple clouds:
+
+* [AWS Elastic Beanstalk](https://github.com/fribeiro1/springframeworksample/tree/master/springframeworksample-awselasticbeanstalk)
+* [GAE](https://github.com/fribeiro1/springframeworksample/tree/master/springframeworksample-gae)
+* [Heroku](https://github.com/fribeiro1/springframeworksample/tree/master/springframeworksample-heroku)
+* [Jelastic](https://github.com/fribeiro1/springframeworksample/tree/master/springframeworksample-jelastic)
+* [OpenShift](https://github.com/fribeiro1/springframeworksample/tree/master/springframeworksample-openshift)
+* [Windows Azure](https://github.com/fribeiro1/springframeworksample/tree/master/springframeworksample-windowsazure)
---
+++ springframeworksample-gae/src/main/java/br/eti/fernandoribeiro/sample/spring/SampleController.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012 Fernando Ribeiro
+ * 
+ * This file is part of Spring Framework Sample.
+ *
+ * Spring Framework Sample is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Spring Framework Sample is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Spring Framework Sample. If not, see <http://www.gnu.org/licenses/>.
+ */
+package br.eti.fernandoribeiro.sample.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.InternalResourceView;
+
+@Controller
+public final class SampleController {
+
+	@RequestMapping("/sendMessage")
+	public ModelAndView sendMessage(
+			@RequestParam("message") final String message) {
+		final ModelAndView modelAndView = new ModelAndView(
+				"/WEB-INF/jsp/confirmation.jsp");
+
+		modelAndView.addObject("message", message);
+
+		return modelAndView;
+	}
+
+	@RequestMapping("/")
+	public View showForm() {
+		return new InternalResourceView("/WEB-INF/jsp/index.jsp");
+	}
+
+}
